6da4c432860dd7083f25352a0fb1675a4be0d7f1
Rachel <rachella@gmail.comOB>
2015-08-02 22:01:31 +0800

    rebase session off of users, merge conflicts

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ server.views({
 var plugins = [
   { register: require('./routes/static-pages.js') },
   { register: require('./routes/users.js') },
+  { register: require('./routes/sessions.js') },
+  { 
+    register: require('yar'),
+    options: {
+      cookieOptions: {
+        password: 'Blahblah',
+        isSecure: false
+      }
+    } 
+  },
   { 
     register: require('hapi-mongodb'),
     options: {
