14ae075468ed2da9e3667a774309f5e6a1324268
Rachel <rachella@gmail.comOB>
2015-08-02 17:22:14 +0800

    feature(users): list users, create new user with validation and password encryption

--- public/application.js
+++ public/application.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+  var name = $('#name');
+  var email = $('#email');  
+  var username2 = $('#username2');
+  var password2 = $('#password2');  
+  
 
+  $('#sign-up').click(function() {
+    event.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/users',
+      data: {
+        user: {      
+          name: name.val(),
+          email: email.val(),
+          username: username2.val(),
+          password: password2.val()
+        }
+      },
+      dataType: 'json',
+      success: function(response) {
+        console.log("Success", response);      
+      }
+    });
+  });
 });
